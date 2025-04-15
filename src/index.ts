1b60f7e87ade043c091a9623305fa92dca0bab02
Mohammed Fadl <mohammadefathl@gmail.com>
2025-04-16 00:47:49 +0300

    chore: use tsx instead of ts-node .

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express from "express";
 const app = express()
 
 app.get("/", (req, res) => {
-	res.send("Hellodw World!")
+	res.send("Hello World!")
 });
 
 app.listen(5000, () => {
-	console.log("Server is running on port " + 5000);
+	console.log(`Server is running on port http://localhost:${5000}`);
 });
